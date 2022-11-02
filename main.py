28d940c91d151be78b47c048b714426a568aa875
broitmanr <broitmanroman@gmail.com>
2022-11-02 19:20:32 -0300

    Se agregan archivos iniciales
    Agrego los archivos del proyecto

---
+++ main.py
@@ -0,0 +1,20 @@
+import cv2
+import math
+from matplotlib import pyplot as plt
+import numpy as np
+import pytesseract
+from PIL import Image
+# # Importa libreria
+#  cap = cv2.VideoCapture("autos.mp4")
+# Ctexto = ''
+#
+# while True:
+#     ret, frame = cap.read()
+#
+#     if ret == False:
+#         break
+#
+#
+#     cv2.imshow()
+cv2.
+# img = cv2.imread("");
---
+++ otro.py
@@ -0,0 +1,70 @@
+import cv2
+import os 
+import glob
+import imutils
+
+cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+majinBooClassif = cv2.CascadeClassifier('cascade.xml')
+while True:
+    
+    ret,frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    toy = majinBooClassif.detectMultiScale(gray,
+    scaleFactor = 5,
+    minNeighbors = 91,
+    minSize=(70,78))
+    for (x,y,w,h) in toy:
+        cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
+        cv2.putText(frame,'Majin Boo',(x,y-10),2,0.7,(0,255,0),2,cv2.LINE_AA)
+    cv2.imshow('frame',frame)
+    
+    if cv2.waitKey(1) == 27:
+        break
+cap.release()
+cv2.destroyAllWindows()
+
+
+
+# Datos = 'p'
+# if not os.path.exists(Datos):
+#     print('Carpeta creada: ',Datos)
+#     os.makedirs(Datos)
+# cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+# x1, y1 = 190, 80
+# x2, y2 = 450, 398
+# count = 0
+# while True:
+#     ret, frame = cap.read()
+#     if ret == False: break
+#     imAux = frame.copy()
+#     cv2.rectangle(frame,(x1,y1),(x2,y2),(255,0,0),2)
+#     objeto = imAux[y1:y2,x1:x2]
+#     objeto = imutils.resize(objeto,width=38)
+#     #print(objeto.shape)
+#     k = cv2.waitKey(1)
+#     if k == ord('s'):
+#         cv2.imwrite(Datos+'/objeto_{}.jpg'.format(count),objeto)
+#         print('Imagen guardada:'+'/objeto_{}.jpg'.format(count))
+#         count = count +1
+#     if k == 27:
+#         break
+#     cv2.imshow('frame',frame)
+#     cv2.imshow('objeto',objeto)
+# cap.release()
+# cv2.destroyAllWindows()
+
+
+
+# img = imread('imgPatentes')
+# cv2.imshow('image',img)
+# Datos= 'P'
+
+# inputFolder = '\imgPatentes\p'
+# if not os.path.exists(Datos):
+#     print("Carpeta creada: ", Datos)
+# i=0
+# for img in glob.glob(inputFolder + "/*.jpg"):
+#     image=imread(img)
+#     cv2.imshow('image',image)
+#     cv2.waitKey(1000)
+# cv2.destroyAllWindows()
